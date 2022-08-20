d183448dfa71d6aaaa4c4ea2ff0eac485b02899c
Dongping_Guo <dguo@horizon.csueastbay.edu>
2022-08-19 23:07:55 -0700

    Add features

--- MyFirstCode.cpp
+++ MyFirstCode.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main(){
-    cout << "Hello World" << endl;
+    cout << "Hello  " << endl;
+    cout << "World" << endl;
+    string name;
+    cin >> name;
+    cout << "h";
     return 0;
 }
